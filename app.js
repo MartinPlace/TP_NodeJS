abb6842b05699efd19e41f68be59e8bc4179c1a9
Martin PLACE <martin@MacBook-Pro.local>
2020-06-29 20:13:28 +0200

    Créez des utilisateurs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -14,7 +15,9 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
+
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 
 
--- models/user.js
+++ models/user.js
@@ -7,5 +7,5 @@ const userSchema = mongoose.Schema({
 });
 
 userSchema.plugin(uniqueValidator);
-
+mongoose.set('useCreateIndex', true);//(node:19907) DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead. // https://github.com/Automattic/mongoose/issues/6890
 module.exports = mongoose.model('User', userSchema);
